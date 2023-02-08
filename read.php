833d4c00a5667948df38b5491ad3c90b152e63e6
Dan <123217422+DannCD@users.noreply.github.com>
2023-02-08 16:07:10 +0800

    Update read.php

--- read.php
+++ read.php
@@ -24,3 +24,33 @@ if (mysqli_num_rows($result) > 0) {
 // Close the connection
 mysqli_close($conn);
 ?>
+<?php
+// Connect to the database
+$conn = mysqli_connect("hostname", "username", "password", "database_name");
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Select all data from the users table
+$sql = "SELECT * FROM users";
+$result = mysqli_query($conn, $sql);
+
+// Check if the query was successful
+if (mysqli_num_rows($result) > 0) {
+    // Output the data
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "Name: " . $row["name"]. "<br>";
+        echo "Age: " . $row["age"]. "<br>";
+        echo "Contact Number: " . $row["contact_number"]. "<br>";
+        echo "Email Address: " . $row["email_address"]. "<br>";
+        echo "Gender: " . $row["gender"]. "<br><br>";
+    }
+} else {
+    echo "0 results";
+}
+
+// Close the connection
+mysqli_close($conn);
+?>
