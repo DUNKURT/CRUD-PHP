082e921be9342f1cadf37d6e616a8ae5da7547fa
Dan <123217422+DannCD@users.noreply.github.com>
2023-02-08 16:13:25 +0800

    Update read.php (#1)

--- read.php
+++ read.php
@@ -7,46 +7,22 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Select all data from the users table
-$sql = "SELECT * FROM users";
+// Get the id from the URL
+$id = $_GET['id'];
+
+// Select the data from the users table with the specified id
+$sql = "SELECT * FROM users WHERE id=$id";
 $result = mysqli_query($conn, $sql);
 
 // Check if the query was successful
 if (mysqli_num_rows($result) > 0) {
     // Output the data
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-
-// Close the connection
-mysqli_close($conn);
-?>
-<?php
-// Connect to the database
-$conn = mysqli_connect("hostname", "username", "password", "database_name");
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-// Select all data from the users table
-$sql = "SELECT * FROM users";
-$result = mysqli_query($conn, $sql);
-
-// Check if the query was successful
-if (mysqli_num_rows($result) > 0) {
-    // Output the data
-    while($row = mysqli_fetch_assoc($result)) {
+    $row = mysqli_fetch_assoc($result);
         echo "Name: " . $row["name"]. "<br>";
         echo "Age: " . $row["age"]. "<br>";
         echo "Contact Number: " . $row["contact_number"]. "<br>";
         echo "Email Address: " . $row["email_address"]. "<br>";
-        echo "Gender: " . $row["gender"]. "<br><br>";
-    }
+        echo "Gender: " . $row["gender"]. "<br>";
 } else {
     echo "0 results";
 }
